2fea8edb1d9e8f64c4a87e23ab1ea5683247a05a
Roeigr7 <roeigr7@gmail.com>
2024-09-12 16:18:25 +0300

    logout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { Auth } from "aws-amplify";
-import { Image, StyleSheet, Platform, TextInput, Alert } from "react-native";
-import { Button } from "react-native-paper";
+import { Image, Platform, StyleSheet, TextInput } from "react-native";
+import { Button, Snackbar } from "react-native-paper";
 import { useDispatch } from "react-redux";
+import { UseSelector } from "react-redux";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { signIn, signOut } from "@/src/redux/features/authSlice";
+import { useAppSelector } from "@/src/redux/store";
 
 type SignUpParameters = {
   username: string;
@@ -45,11 +47,14 @@ type SignUpParameters = {
 // }
 
 export default function HomeScreen() {
+  const auth = useAppSelector((state) => state.auth);
   const dispatch = useDispatch(); // Create dispatch hook for Redux actions
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   // const handleSignUp = () => {
   //   SignUp({ username, password, email, phone_number: phoneNumber }, dispatch);
@@ -57,97 +62,108 @@ export default function HomeScreen() {
 
   const handleSignIn = async () => {
     try {
+      setLoading(true);
       const user = await Auth.signIn(username, password);
       console.log("Signed in user:", user);
       dispatch(
         signIn({ username: user.username, userDataKey: user.userDataKey })
       );
-      Alert.alert("Success", `User ${username} signed in successfully`);
     } catch (error) {
+      setLoading(false);
       console.log("Error signing in:", error);
-      Alert.alert("Error", "Failed to sign in: " + error);
+      setError(true);
     }
+    setLoading(false);
   };
 
   const handleSignOut = async () => {
+    console.log("123");
     try {
-      await Auth.signOut();
-      dispatch(signOut()); // Dispatch logout action
-      Alert.alert("Success", "User signed out successfully");
+      await Auth.signOut({ global: true });
+      dispatch(signOut());
     } catch (error) {
-      console.log("Error signing out:", error);
-      Alert.alert("Error", "Failed to sign out: " + error);
+      console.log("error signing out: ", error);
     }
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-
-      {/* Show login status */}
-      {username && (
-        <ThemedView style={styles.loginStatus}>
-          <ThemedText>User {username} is logged in!</ThemedText>
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+        headerImage={
+          <Image
+            source={require("@/assets/images/partial-react-logo.png")}
+            style={styles.reactLogo}
+          />
+        }
+      >
+        <ThemedView style={styles.titleContainer}>
+          <HelloWave />
         </ThemedView>
+
+        {/* Show login status */}
+
+        {/* Signup Form */}
+        <ThemedView style={styles.formContainer}>
+          <TextInput
+            placeholder="Username"
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            placeholder="Phone Number"
+            style={styles.input}
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+          <Button loading={loading} onPress={handleSignIn}>
+            SIGN up
+          </Button>
+          <Button
+            loading={loading}
+            icon="camera"
+            mode="contained"
+            onPress={handleSignIn}
+          >
+            login
+          </Button>
+          <Button loading={loading} onPress={handleSignOut}>
+            Logout
+          </Button>
+        </ThemedView>
+
+        {/* Other Content */}
+        <ThemedView style={styles.stepContainer}>
+          <ThemedText type="subtitle">Step 1:</ThemedText>
+          <ThemedText type="title">{`Welcome! ${
+            auth.isAuth ? auth.username : "no"
+          }`}</ThemedText>
+        </ThemedView>
+      </ParallaxScrollView>
+      {error && (
+        <Snackbar
+          duration={50}
+          onDismiss={() => console.log("dis")}
+          visible={error}
+        >
+          Hey there! I'm a Snackbar.
+        </Snackbar>
       )}
-
-      {/* Signup Form */}
-      <ThemedView style={styles.formContainer}>
-        <TextInput
-          placeholder="Username"
-          style={styles.input}
-          value={username}
-          onChangeText={setUsername}
-        />
-        <TextInput
-          placeholder="Password"
-          style={styles.input}
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          placeholder="Phone Number"
-          style={styles.input}
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-        />
-        <Button onPress={handleSignIn}>SIGN up</Button>
-        <Button icon="camera" mode="outlined" onPress={handleSignIn}>
-          login
-        </Button>
-        <Button onPress={handleSignOut}>Logout</Button>
-      </ThemedView>
-
-      {/* Other Content */}
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          <ThemedText type="defaultSemiBold">{username} is log in</ThemedText>{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    </>
   );
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+
 import authReducer from "./features/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
-    authReducer,
+    auth: authReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = ReturnType<typeof store.dispatch>;
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
